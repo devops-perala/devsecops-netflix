45a913aadabd911a033901c16241319c2f63bac4
DevOps-Factory <factorydevops@gmail.com>
2025-07-07 10:40:51 +0530

    Update backend.tf

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "netflix-perala-bucket" # Replace with your actual S3 bucket name
+    bucket = "netflix-perala-buckets" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
